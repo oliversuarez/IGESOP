973599fcaea3b8a6be477f5743fd9599cdf26fb5
Huaraca <dhuaraca@munizlaw.com>
2022-06-22 19:12:13 -0500

    segundo cambio encuesta

--- evaluacionDesempenno/Scripts/Evaluacion/evaluacion.js
+++ evaluacionDesempenno/Scripts/Evaluacion/evaluacion.js
@@ -1,4 +1,11 @@
-var listaEval=[];
+var listaEval = [];
+var titulo;
+var listaCompetencia;
+var listaObjetivo;
+var listaLeyenda;
+var listaUsu;
+
+
 window.onload = function () {
     Http.get("evaluacion/cargar", mostrarRpt);
 
@@ -7,6 +14,117 @@ window.onload = function () {
 
 function mostrarRpt(rpta) {
     if (validaResponseData(rpta)) {
-        listaEval = rpta.split(sepLista)
+        listaEval = rpta.split(sepLista);
+        titulo = listaEval[0];
+        listaCompetencia = listaEval[1].split(sepRegistros);
+        listaObjetivo = listaEval[2].split(sepRegistros);
+        listaLeyenda = listaEval[3].split(sepRegistros);
+        listaUsu = listaEval[4].split(sepRegistros);
+        crearEvaluacion();
     }     
 }
+function crearEvaluacion() {
+    titulo_evaluacion.innerHTML = titulo;
+    var descripcion= crearSubtituloResena(listaCompetencia[0].split(sepCampos)[0], listaCompetencia[0].split(sepCampos)[1]);
+    listaCompetencia.splice(0, 1);
+    crearTablaOpcionMultiple(listaLeyenda, listaCompetencia, evaluacion_evaluacion, descripcion);
+    var ListaOpciones = ['70%', '80%', '100%', '110%', '120%','150%'];
+    var descripcion2 = crearSubtituloResena(listaObjetivo[0].split(sepCampos)[0], listaObjetivo[0].split(sepCampos)[1]);
+    listaObjetivo.splice(0, 1);
+    crearTablaOpcionMultiple(ListaOpciones, listaObjetivo, objetivo_evaluacion, descripcion2);
+}
+
+function crearSubtituloResena(titulo,descripcion) {
+
+    var html = "";
+    html += "<div class='title'>";
+    html += titulo;
+    html += "</div>";
+    html += "<div class='texto'>";
+    html += descripcion;
+    html += "</div>";
+    return html;
+    
+}
+
+function crearTablaOpcionMultiple(listaOpciones,listaData,div,descripcion) {
+    var campos = [];
+    var opCampos = [];
+    var nlistaData = listaData.length;
+    var nlistaOpciones = listaOpciones.length;
+    var html = "";
+    var matriz = [];
+    var listaCabecera = listaData[0].split(sepCampos);
+    var nlistaCabecera = listaCabecera.length;
+    html += descripcion;
+  html += "<table class='table'>";
+   html += "<thead class='thead'>";
+    html += "<tr>";
+    html += "<th>";
+    html += "#";
+    html += "</th>";
+
+    for (var i = 0; i < nlistaCabecera; i++) {
+        html += "<th>";
+        html += listaCabecera[i];
+        html += "</th>";
+    }
+   
+    for (var i = 0; i < nlistaOpciones; i++) {
+        html += "<th style='width:50px;'>";
+        html += listaOpciones[i].split(sepCampos)[0];
+        html += "</th>"
+
+    }
+      html += "</tr>";
+    html += "</thead>";
+    html += "<tbody class='tbody'>";
+    for (var i = 1; i < nlistaData; i++) {
+        campos = listaData[i].split(sepCampos);
+       /* matriz[i] = "";*/
+        html += "<tr>";
+        html += "<td>";
+        html += i+1;
+        html += "</td>";
+        for (var j = 0; j < nlistaCabecera; j++) {
+            html += "<td>";
+            html += campos[j];
+            html += "</td>";
+        }
+       
+ 
+        //matriz[i][0] = campos[0];
+        //matriz[i][1] = campos[1];
+        //matriz[i][2] = campos[2];
+        for (var j = 0; j < nlistaOpciones; j++) {
+            opCampos = listaOpciones[j].split(sepCampos);
+      /*      matriz[i][nlistaData + j] = [];*/
+            html += "<td class='";
+            html += campos[0];
+            html+="opcionCheck' data-valor='";
+            html += opCampos[0];
+            html +="'>";
+  
+            html += "</td>"
+            //matriz[i][nlistaData + j][0] = opCampos[0];
+            //matriz[i][nlistaData + j][1] = false;
+        }
+        html += "</tr>";
+    }
+    html += "</tbody>";
+    html += "</table>";
+    div.innerHTML = html;
+
+    function configurarCheck() {
+        var listaControl = [];
+        for (var i = 1; i < nlistaData; i++) {
+            var 
+            listaControl=  div.getElementsByClassName(nlistaData[i][0] + "opcionCheck");
+        }
+      
+    }
+}
+
+
+
+
--- evaluacionDesempenno/Scripts/Sistema/Index.js
+++ evaluacionDesempenno/Scripts/Sistema/Index.js
@@ -1,5 +1,18 @@
 ﻿btnAceptar.onclick = function () {
     var url = hdfRaiz.value + 'Sistema/Principal';
     window.location.href = url;
+    var text = txtUsuario.value;
+    digestMessage(text)
+        .then(digestHex => console.log(digestHex));
 
+}
+
+
+
+async function digestMessage(message) {
+    const msgUint8 = new TextEncoder().encode(message);                           
+    const hashBuffer = await crypto.subtle.digest('SHA-512', msgUint8);           
+    const hashArray = Array.from(new Uint8Array(hashBuffer));                     
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); 
+    return hashHex;
 }
